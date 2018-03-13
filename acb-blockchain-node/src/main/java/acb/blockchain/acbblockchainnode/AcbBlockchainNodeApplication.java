3b3ea49a5d4b4b9d81cd2f5a014542d4a0fc9ccb
unknown <liuyck@digitalchina.com>
2018-03-13 18:41:18 +0800

    简单区块链搭建

---
+++ acb-blockchain-node/src/main/java/acb/blockchain/acbblockchainnode/AcbBlockchainNodeApplication.java
@@ -0,0 +1,12 @@
+package acb.blockchain.acbblockchainnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcbBlockchainNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcbBlockchainNodeApplication.class, args);
+	}
+}
---
+++ acb-blockchain-node/src/main/java/acb/blockchain/entity/Block.java
@@ -0,0 +1,68 @@
+package acb.blockchain.entity;
+
+
+public class Block {
+
+    /**是这个块在整个链中的位置*/
+
+    private int index;
+
+    /**显而易见就是块生成时的时间戳*/
+
+    private String timestamp;
+
+    /**虚拟资产。我们要记录的数据*/
+
+    private int vac;
+
+    /**是这个块通过 SHA256 算法生成的散列值*/
+
+    private String hash;
+
+    /**指向前一个块的 SHA256 散列值*/
+
+    private String prevHash;
+    
+    /** getters and setters**/ 
+    
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getVac() {
+		return vac;
+	}
+
+	public void setVac(int vac) {
+		this.vac = vac;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public String getPrevHash() {
+		return prevHash;
+	}
+
+	public void setPrevHash(String prevHash) {
+		this.prevHash = prevHash;
+	}
+
+}
